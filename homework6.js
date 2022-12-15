871894d391ee96f2066bd0514dedf23fc0f751fd
Evgenii <pozar1288@gmail.com>
2022-12-15 10:40:24 +0200

    HOMEWORK6

---
+++ homework6.js
@@ -0,0 +1,89 @@
+// task1
+
+// let id = document.getElementById("test").innerHTML = "last";
+
+// let id = document.querySelector("#test");
+// id.textContent = "last";
+
+// task2
+
+// let img = document.querySelector(".image");
+// img.src = 'cat.jpg';
+// alert(img.outerHTML); 
+
+// task3
+
+// let div = document.querySelectorAll('div > p');
+// let counter = 0;
+// div.forEach(elem => {
+//     console.log(`Selector text ${counter} : ${elem.innerHTML}`);
+//     counter++;
+// });
+
+// task4
+
+// let list = document.querySelectorAll("#list > li");
+// alert(`${list[0].innerHTML}, ${list[4].innerHTML}, ${list[1].innerHTML}, ${list[3].innerHTML}, ${list[2].innerHTML}`);
+
+// let list = document.getElementById('list');
+// alert(`${list.children[0].innerHTML}, ${list.children[4].innerHTML}, ${list.children[1].innerHTML}, ${list.children[3].innerHTML}, ${list.children[2].innerHTML}`);
+
+// task5
+
+// let header = document.querySelector('h1');
+// header.style.backgroundColor = 'green';
+
+// let paragraph = document.querySelectorAll('div > p');
+// paragraph[0].style.fontWeight = 'bold';
+// paragraph[1].style.color = 'red';
+// paragraph[2].style.textDecoration = 'underline';
+// paragraph[3].style.fontStyle = 'italic';
+
+// let horizontal = document.getElementById('myList')
+// horizontal.style.display = 'flex'
+// let horizontalItem = document.querySelectorAll('ul > li')
+// horizontalItem[0].style.listStyleType = 'none' 
+// horizontalItem[1].style.listStyleType = 'none'
+// horizontalItem[2].style.listStyleType = 'none'
+
+// let invisible = document.querySelector('span');
+// invisible.style.display = 'none';
+
+// task6
+
+// const firstInput = document.getElementById('input1');
+// const secondInput = document.getElementById('input2');
+
+// firstInput.value = prompt('First input');
+// secondInput.value = prompt("Second input");
+
+// [firstInput.value, secondInput.value] = [secondInput.value, firstInput.value]
+
+
+// task7
+
+// const main = document.createElement('main');
+// main.classList.add('mainClass');
+// main.classList.add('check');
+// main.classList.add('item');
+
+// const body = document.body;
+// body.appendChild(main)
+
+// const div = document.createElement('div');
+// div.id = 'myDiv';
+// main.appendChild(div)
+
+// const paragraph = document.createElement('p');
+// div.appendChild(paragraph);
+
+// const text = document.createTextNode('First paragraph');
+// paragraph.appendChild(text)
+
+// console.log(main.outerHTML)
+
+
+
+
+
+
